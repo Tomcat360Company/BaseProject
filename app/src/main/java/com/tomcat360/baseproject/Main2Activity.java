1622965631c38f1c2aee6f01f882771c010ecc73
dingpengqiang <dingpengqiang>
2017-12-18 16:34:19 +0800

    删除无用东西

--- app/src/main/java/com/tomcat360/baseproject/Main2Activity.java
+++ app/src/main/java/com/tomcat360/baseproject/Main2Activity.java
@@ -17,34 +17,7 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        initData();
+
     }
-
-    private void initData() {
-
-        HashMap<String, String> map = new HashMap<>();
-        map.put("type", 1 + "");
-
-        RxService.createApi(TestApi.class).getData(map)
-                .compose(RxIoMain._io_main(this))
-
-                .subscribe(new NetCheckerSubscriber<Object>(this) {
-                    @Override
-                    protected void onSuccess(Object o) {
-                        Logger.e("1成功");
-                    }
-
-                    @Override
-                    protected void onFailed(String message) {
-                        Logger.e("2失败:" + message);
-                    }
-
-                    @Override
-                    protected void onFinish() {
-                        Logger.e("3完成");
-                    }
-                });
-    }
-
 
 }
